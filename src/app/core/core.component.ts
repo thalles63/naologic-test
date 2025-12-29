472914238d8ffa7a9e4ded37d404339f253c7e8f
Thalles Leonardelli <leonardellit@delagelanden.com>
2025-12-29 11:37:46 -0300

    Create header

--- src/app/core/core.component.ts
+++ src/app/core/core.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { HeaderComponent } from "../components/header/header.component";
 
 @Component({
     selector: "app-core",
     standalone: true,
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, HeaderComponent],
     templateUrl: "./core.component.html"
 })
 export class CoreComponent {}
--- src/app/core/core.config.ts
+++ src/app/core/core.config.ts
@@ -1,5 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
+import { provideRouter } from "@angular/router";
+import { routes } from "./core.routes";
 
 export const CoreConfig: ApplicationConfig = {
-    providers: [provideZoneChangeDetection({ eventCoalescing: true })]
+    providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
